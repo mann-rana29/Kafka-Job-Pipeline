e285d13d62c6c5c28073fe81f7960d161fe062fa
the25thjin <mannr075@gmail.com>
2026-06-11 16:21:07 +0530

    initialized job submission service

---
+++ job-submission-service/src/main/java/main/jobsubmissionservice/JobSubmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package main.jobsubmissionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSubmissionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSubmissionServiceApplication.class, args);
+    }
+
+}
---
+++ job-submission-service/src/test/java/main/jobsubmissionservice/JobSubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package main.jobsubmissionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSubmissionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
