9fce9a8db03174ec09a592dbad82f329a42c3e15
the25thjin <mannr075@gmail.com>
2026-06-16 15:55:50 +0530

    initialized worker  service

---
+++ worker-service/src/test/java/main/workerservice/WorkerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package main.workerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
