6db06005e2241fe302148bc82c696984e33d5a5d
the25thjin <mannr075@gmail.com>
2026-06-17 18:00:06 +0530

    added retry logic

--- worker-service/src/main/java/main/workerservice/consumers/RetryConsumer.java
+++ worker-service/src/main/java/main/workerservice/consumers/RetryConsumer.java
@@ -2,8 +2,6 @@ package main.workerservice.consumers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import main.workerservice.enums.JobStatus;
-import main.workerservice.enums.JobType;
 import main.workerservice.models.JobEvent;
 import main.workerservice.services.JobProcessingService;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -15,30 +13,38 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-@Component
-@Slf4j
+import java.util.concurrent.ThreadLocalRandom;
+
 @RequiredArgsConstructor
-public class JobConsumer {
+@Slf4j
+@Component
+public class RetryConsumer {
     private final RedisTemplate<String,Object> redisTemplate;
     private final JobProcessingService jobProcessingService;
 
-    @KafkaListener(topics = "jobs", groupId = "pipeline-worker-group")
-    public void consume(
+    @KafkaListener(topics = "jobs.retry", groupId = "pipeline-worker-retry-group")
+    public void consumeRetry(
             @Payload JobEvent event,
-            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
             @Header(KafkaHeaders.OFFSET) long offset,
+            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
             Acknowledgment ack
-    ){
-        String redisKey = "job:" + event.getJobId();
+            ){
 
-        String currentStatus = (String) redisTemplate.opsForHash().get(redisKey,"status");
-        if("DONE".equals(currentStatus) || "FAILED".equals(currentStatus)){
-            log.info("Job {} already processed with status: {}. Skipping Execution.", event.getJobId(), currentStatus);
-            ack.acknowledge();
-            return;
+        long waitMs = (long) (Math.pow(2,event.getAttemptNumber() -1) * 1000);
+        long jitter = ThreadLocalRandom.current().nextLong(0,500);
+
+        waitMs += jitter;
+
+        log.info("Retry attempt {} for job {} - waited {}ms on partition {} at offset {}", event.getAttemptNumber(), event.getJobId(), waitMs, partition,offset );
+
+        try{
+            Thread.sleep(waitMs);
+
+            jobProcessingService.processAndRoute(event,ack);
         }
-
-        log.info("Main consumer pulled job id {} from partition {} at offset {}", event.getJobId(),partition,offset);
-        jobProcessingService.processAndRoute(event,ack);
+        catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+            log.error("Retry backoff delay thread was interrupted for job Id : {} ", event.getJobId(),e);
+        }
     }
 }
---
+++ worker-service/src/main/java/main/workerservice/services/JobProcessingService.java
@@ -0,0 +1,91 @@
+package main.workerservice.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import main.workerservice.enums.JobStatus;
+import main.workerservice.models.JobEvent;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class JobProcessingService {
+    private final RedisTemplate<String,Object> redisTemplate;
+    private final KafkaTemplate<String, JobEvent> kafkaTemplate;
+
+    public void processAndRoute(JobEvent event, Acknowledgment ack){
+        String redisKey = "job:" + event.getJobId();
+
+        try{
+            redisTemplate.opsForHash().put(redisKey,"status", JobStatus.PROCESSING.name());
+            log.info("Job {} state updated to PROCESSING in Redis", event.getJobId());
+
+            // simulating a process
+            processJob(event);
+
+            if(event.getJobId().toString().endsWith("0")) throw new RuntimeException("Simulated processing failed!");
+
+            redisTemplate.opsForHash().put(redisKey,"status",JobStatus.DONE.name());
+            String outputUrl = String.format("https://storage/%s/%s.output",event.getJobType() , event.getJobId());
+            redisTemplate.opsForHash().put(redisKey,"result",outputUrl);
+            log.info("Job {} successfully completed processing! Output URL stored. ", event.getJobId());
+
+            ack.acknowledge();
+        }
+        catch (RuntimeException e){
+            log.error("Processing failed for job Id: {}",event.getJobId());
+
+            int currentAttempts = event.getAttemptNumber();
+            int maxAttempts = event.getMaxAttempts();
+
+            if(currentAttempts < maxAttempts){
+                event.setAttemptNumber(currentAttempts+1);
+                event.setLastError(e.getMessage());
+
+                kafkaTemplate.send("jobs.retry",event.getJobId().toString(),event);
+                log.info("Job Id {} failed (Attempt {} / {}. Forwarded to jobs.retry.", event.getJobId(),event.getAttemptNumber(),event.getMaxAttempts());
+
+                redisTemplate.opsForHash().put(redisKey,"status", JobStatus.PENDING.name());
+                ack.acknowledge();
+            }
+            else{
+                event.setLastError(e.getMessage());
+
+                kafkaTemplate.send("jobs.dlq", event.getJobId().toString(),event);
+                log.error("Job Id: {} completely failed after {} attempts. Forwarded to jobs.dlq", event.getJobId(), maxAttempts);
+
+                redisTemplate.opsForHash().put(redisKey,"status",JobStatus.FAILED.name());
+                redisTemplate.opsForHash().put(redisKey,"error",e.getMessage());
+                ack.acknowledge();
+
+            }
+        }
+        catch (Exception e){
+            log.error("Failed processing the job Id : {}. ", event.getJobId(),e);
+        }
+    }
+    private void processJob(JobEvent event) throws InterruptedException{
+        switch (event.getJobType()){
+            case PDF_GENERATION :
+                log.info("Processing PDF Generation task for job Id: {}",event.getJobId());
+                Thread.sleep(4000);
+                break;
+
+            case IMAGE_RESIZE :
+                log.info("Resizing image for Job id: {}",event.getJobId());
+                Thread.sleep(10000);
+                break;
+
+            case DATA_EXPORT :
+                log.info("Processing Data Export task for job Id: {}",event.getJobId());
+                Thread.sleep(2000);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unsupported Job execution path: " + event.getJobType());
+        }
+    }
+}
