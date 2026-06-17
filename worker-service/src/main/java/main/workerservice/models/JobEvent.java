165bc69d0ff4d78f3e456d80d46719dc577dcc6a
the25thjin <mannr075@gmail.com>
2026-06-17 17:05:04 +0530

    created job consumer file

---
+++ worker-service/src/main/java/main/workerservice/models/JobEvent.java
@@ -0,0 +1,25 @@
+package main.workerservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import main.workerservice.enums.JobStatus;
+import main.workerservice.enums.JobType;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobEvent {
+    private UUID jobId;
+    private JobType jobType;
+    private JobStatus jobStatus;
+    private String submittedBy;
+    private long submittedAt;
+    private int attemptNumber;
+    private int maxAttempts;
+    private Map<String,Object> payload;
+    private String lastError;
+}
